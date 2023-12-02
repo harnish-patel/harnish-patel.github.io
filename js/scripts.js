9f7d1c22fe7929d1e6327919ca0bc275c876783f
Harnish Patel <35270643+harnish-patel@users.noreply.github.com>
2023-12-01 22:12:00 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Function to handle card expansion
+    function expandCard() {
+        const card = document.getElementById('myCard');
+        // Toggle the max-height property to expand or collapse the card
+        card.style.maxHeight = card.style.maxHeight === '200px' ? '500px' : '200px';
+    }
+
+});
